44a518f114822176e925d583b12df7b3216ae87e
danbart <danilo_solorzano06@hotmail.com>
2020-03-31 19:16:45 -0600

    Aplicación React con Typescript

---
+++ docs/precache-manifest.83719d23916c90e13d9e7e8ca1a9a273.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73dba4124191db1d605856551ecb5834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29efb306a782ece4a047",
+    "url": "/static/css/2.b5efd36a.chunk.css"
+  },
+  {
+    "revision": "53920b6070a05e05bb95",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29efb306a782ece4a047",
+    "url": "/static/js/2.d44d053f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d44d053f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53920b6070a05e05bb95",
+    "url": "/static/js/main.350d3c62.chunk.js"
+  },
+  {
+    "revision": "e102be6b9c1363c2f00c",
+    "url": "/static/js/runtime-main.3aea5244.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
-function App() {
+type FormElement = React.FormEvent<HTMLFormElement>
+interface iTask {
+  name: string,
+  done: boolean
+}
+
+function App(): JSX.Element {
+
+  const [newTask, setNewTask] = useState<string>('')
+  const [tasks, setTask] = useState<iTask[]>([])
+  const taskInput = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault()
+    addTask(newTask)
+    setNewTask('')
+  }
+
+  const addTask = (name: string) => {
+    const newTasks = [...tasks, { name, done: false }]
+    setTask(newTasks)
+    taskInput.current?.focus()
+  }
+
+  const toggleDoneTaskt = (i: number) => {
+    const newTasks: iTask[] = [...tasks]
+    newTasks[i].done = !newTasks[i].done
+    setTask(newTasks)
+  }
+
+  const removeTastk = (i: number) => {
+    const newTasks: iTask[] = [...tasks]
+    newTasks.splice(i, 1)
+    setTask(newTasks)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  onChange={e => setNewTask(e.target.value)}
+                  className="form-control"
+                  value={newTask}
+                  ref={taskInput}
+                  autoFocus />
+                <button className="btn btn-success btn-block mt2">Save</button>
+              </form>
+            </div>
+          </div>
+          {
+            tasks.map((t: iTask, i: number) => (
+              <div className="card card-body mt-2" key={i}>
+                <h2 style={{ textDecoration: t.done ? 'line-through' : '' }}>{t.name}</h2>
+                <div>
+                  <button className="btn btn-secondary" onClick={() => toggleDoneTaskt(i)}>
+                    {t.done ? '✓' : '✗'}
+                  </button>
+                  <button className="btn btn-danger" onClick={() => removeTastk(i)}>
+                  🗑️
+                  </button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
